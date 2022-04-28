2293c5960dde560a5d52605c0f2428e67933e681
httpbunny97 <gabriel_valentim17@hotmail.com>
2022-04-28 08:11:04 -0300

    teste

---
+++ js/index.js
@@ -0,0 +1,19 @@
+$(document).ready(function() { 
+    $('.sidenav').sidenav({
+        edge: 'right'
+    });
+    $('.tabs').tabs();
+
+    $('.materialboxed').materialbox();
+
+    $('.nav a[href^="#"]').on('click', function(e) {
+        e.preventDefault();
+        var id = $(this).attr('href'),
+            targetOffset = $(id).offset().top;
+
+        $('html, body').animate({
+            scrollTop: targetOffset
+        }, 1000);
+    });
+
+});
