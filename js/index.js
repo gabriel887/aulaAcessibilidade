1d0dd07d06d3969a42811da36a8b9e1b7a9fd4f0
gabriel887 <49587014+gabriel887@users.noreply.github.com>
2022-05-30 22:55:20 -0300

    correções e tals

--- js/index.js
+++ js/index.js
@@ -1,4 +1,18 @@
-$(document).ready(function() { 
+document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.modal');
+    var instances = M.Modal.init(elems, "");
+});
+
+function confirmar() {
+    document.getElementById("nomeConf").value = document.getElementById("nome").value
+    document.getElementById("emailConf").value = document.getElementById("email").value
+    document.getElementById("nascConf").value = document.getElementById("nasc").value
+    document.getElementById("mensagemConf").innerHTML = document.getElementById("mensagem").value
+    var instance = M.Modal.getInstance(document.getElementById("modalForm"));
+    instance.open();
+}
+
+$(document).ready(function() {
     $('.sidenav').sidenav({
         edge: 'right'
     });
@@ -17,3 +31,13 @@ $(document).ready(function() {
     });
 
 });
+
+function changeImage() {
+    srcAtual = document.getElementById("gifVelhinho").src;
+    if (srcAtual.includes("img/gifVelhinho.gif")) {
+        document.getElementById("gifVelhinho").src = "img/gifVelhinhoParado.png"
+    } else {
+        document.getElementById("gifVelhinho").src = "img/gifVelhinho.gif"
+
+    }
+}
